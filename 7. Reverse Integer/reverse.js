f94408b8bff88b0195ca0352acb9de683a1f39c3
21lakshh <lakshyapaliwalodd@gmail.com>
2025-05-16 13:01:13 +0530

    Updated Sol

--- 7. Reverse Integer/reverse.js
+++ 7. Reverse Integer/reverse.js
@@ -1,10 +1,21 @@
 var reverse = function(x) {
     let reversednum = 0
+    let temp = 0
+    if(x<0){
+        temp = x
+        x = Math.abs(x)
+    }
     while(x!=0){
         reversednum = reversednum * 10 + x%10
         x = Math.floor(x / 10)
     }
+    if(temp<0){
+        reversednum = -(reversednum)
+    }
+    if(reversednum < Math.pow(-2, 31) || reversednum > Math.pow(2,31) -1){
+        return 0
+    }
     return reversednum
 };
 
-console.log(reverse(523))
+console.log(reverse(-523))
