1ea79a08fb8a2ff9237e0e64958665a5782d6af8
21lakshh <lakshyapaliwalodd@gmail.com>
2025-05-24 23:09:14 +0530

    New

---
+++ 189. Rorate Array/rotate.js
@@ -0,0 +1,20 @@
+var rotate = function(nums, k) {
+    k = k % nums.length
+
+    function reverse(nums, start, end){
+        while (start<end) {
+            [nums[start], nums[end]] = [nums[end], nums[start]]
+            start++
+            end--
+        }
+    }
+
+    reverse(nums,0,nums.length-1)
+    reverse(nums,0,k-1)
+    reverse(nums,k,nums.length-1)
+
+    return nums
+
+};
+
+console.log(rotate([-1,-100,3,99],2))
